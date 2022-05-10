d5393cc7740b24751d4151b7f7270860aab71e21
Ainhoa <ainhoaopez99.al@gmail.com>
2022-05-10 14:25:46 +0200

    Boton cerrar sesion

--- SesionControl.js
+++ SesionControl.js
@@ -1,6 +1,32 @@
-/*document.querySelector('body').addEventListener('load', () => {
-    if(localStorage){
-        if(localStorage.getItem('Estado')===1){
+const SesionControl = () => {
+    if (localStorage.length > 0){
+        const estado = localStorage.getItem('Estado');
+        if(estado === '1' ){
+            document.getElementById('inicio-sesion').classList.add('desactivo');
+            document.getElementById('registro-sesion').classList.add('desactivo');
+            document.getElementById('cerrar-sesion').classList.remove('desactivo');
+            const cerrar_sesion = document.getElementById('cerrar_sesion');
+            cerrar_sesion.addEventListener('click', CerrarSesion);
+        } else{
+            document.getElementById('inicio-sesion').classList.remove('desactivo');
+            document.getElementById('registro-sesion').classList.remove('desactivo');
+            document.getElementById('cerrar-sesion').classList.add('desactivo');
         }
+    } else{
+        document.getElementById('inicio-sesion').classList.remove('desactivo');
+        document.getElementById('registro-sesion').classList.remove('desactivo');
+        document.getElementById('cerrar-sesion').classList.add('desactivo');
     }
-}*/
+}
+
+
+
+const CerrarSesion = () =>{
+    localStorage.removeItem('Estado');
+    localStorage.setItem("Estado", 0);
+    document.getElementById('inicio-sesion').classList.remove('desactivo');
+    document.getElementById('registro-sesion').classList.remove('desactivo');
+    document.getElementById('cerrar-sesion').classList.add('desactivo');
+}
+    
+
