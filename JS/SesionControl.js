f397e53855ca868afc50950f6191088c1a1d31c5
Ainhoa99 <ainhoalopez99.al@gmail.com>
2022-05-11 21:52:23 +0200

    Cambios

--- JS/SesionControl.js
+++ JS/SesionControl.js
@@ -1,4 +1,4 @@
-// const cerrar_sesion = document.getElementById('cerrar_sesion');
+// const btn = document.getElementById('cerrar_sesion')
 
 
 const SesionControl = () => {
@@ -8,7 +8,7 @@ const SesionControl = () => {
             document.getElementById('inicio-sesion').classList.add('desactivo');
             document.getElementById('registro-sesion').classList.add('desactivo');
             document.getElementById('cerrar-sesion').classList.remove('desactivo');
-            document.getElementById('usuario-sesion').innerHTML= ('(' + localStorage.getItem('Nombre') + ')');
+            document.getElementById('usuario-sesion').innerHTML= ('Usuario registrado: ' + localStorage.getItem('Nombre'));
             document.getElementById('usuario-sesion').classList.remove('desactivo');
             // cerrar_sesion.addEventListener('click', CerrarSesion);
         } else{
@@ -27,17 +27,7 @@ const SesionControl = () => {
 }
 
 
-
-// const CerrarSesion = () =>{
-//     localStorage.removeItem('Estado');
-//     localStorage.setItem("Estado", 0);
-//     document.getElementById('inicio-sesion').classList.remove('desactivo');
-//     document.getElementById('registro-sesion').classList.remove('desactivo');
-//     document.getElementById('cerrar-sesion').classList.add('desactivo');
-// }
-
-
-// cerrar_sesion.addEventListener('click', (e)=> {
+// btn.addEventListener('click', (e)=> {
 //     e.defaultPrevented();
 //     localStorage.removeItem('Estado');
 //     localStorage.setItem("Estado", 0);
