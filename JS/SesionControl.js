c4b64dc3276b1916add03d8a59f735ee4d7dac29
Ainhoa <ainhoaopez99.al@gmail.com>
2022-05-13 14:32:32 +0200

    botonCerrarSesionHabilitado

--- JS/SesionControl.js
+++ JS/SesionControl.js
@@ -1,38 +1,45 @@
-// const btn = document.getElementById('cerrar_sesion')
-
-
 const SesionControl = () => {
     if (localStorage.length > 0){
         const estado = localStorage.getItem('Estado');
         if(estado === '1' ){
             document.getElementById('inicio-sesion').classList.add('desactivo');
             document.getElementById('registro-sesion').classList.add('desactivo');
-            document.getElementById('cerrar-sesion').classList.remove('desactivo');
-            document.getElementById('usuario-sesion').innerHTML= ('Usuario registrado: ' + localStorage.getItem('Nombre'));
-            document.getElementById('usuario-sesion').classList.remove('desactivo');
-            // cerrar_sesion.addEventListener('click', CerrarSesion);
+            document.getElementById('btnCerrarSesion').classList.remove('desactivo');
+            document.getElementById('btnEliminarCuentas').classList.remove('desactivo');
+
+
+            const btnCerrarSesion = document.getElementById('btnCerrarSesion')
+            const btnEliminarCuentas = document.getElementById('btnEliminarCuentas')
+
+            btnCerrarSesion.addEventListener('click', CerrarSesion);
+            btnEliminarCuentas.addEventListener('click', EliminarCuentas);
+
         } else{
             document.getElementById('inicio-sesion').classList.remove('desactivo');
             document.getElementById('registro-sesion').classList.remove('desactivo');
-            document.getElementById('cerrar-sesion').classList.add('desactivo');
-            document.getElementById('usuario-sesion').classList.add('desactivo');
+            document.getElementById('btnCerrarSesion').classList.add('desactivo');
+            document.getElementById('btnEliminarCuentas').classList.add('desactivo');
         }
     } else{
         document.getElementById('inicio-sesion').classList.remove('desactivo');
         document.getElementById('registro-sesion').classList.remove('desactivo');
-        document.getElementById('cerrar-sesion').classList.add('desactivo');
-        document.getElementById('usuario-sesion').classList.add('desactivo');
+        document.getElementById('btnCerrarSesion').classList.add('desactivo');
+        document.getElementById('btnEliminarCuentas').classList.add('desactivo');
 
     }
+};
+
+
+const CerrarSesion = () => {
+    localStorage.removeItem('Estado');
+    localStorage.setItem("Estado", 0);
+    document.getElementById('inicio-sesion').classList.remove('desactivo');
+    document.getElementById('registro-sesion').classList.remove('desactivo');
+    document.getElementById('btnEliminarCuentas').classList.add('desactivo');
 }
 
-
-// btn.addEventListener('click', (e)=> {
-//     e.defaultPrevented();
-//     localStorage.removeItem('Estado');
-//     localStorage.setItem("Estado", 0);
-//     document.getElementById('inicio-sesion').classList.remove('desactivo');
-//     document.getElementById('registro-sesion').classList.remove('desactivo');
-//     document.getElementById('cerrar-sesion').classList.add('desactivo');
-// })
+const EliminarCuentas = () => {
+    alert(localStorage.length + " Cuentas eliminadas")
+    localStorage.clear();
+}
 
